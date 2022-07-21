74ec8c2f8a70711d4a730c6c7c28a05503dd946a
fchristovam <fchristovam7@gmail.com>
2022-07-21 17:31:11 -0300

    insere comentários no dicas-controllers.js

--- src/controllers/dicas-controllers.js
+++ src/controllers/dicas-controllers.js
@@ -1,11 +1,13 @@
 class Dicas {
     rotas(app){
         app.get("/dicas", (req, res) => {
-           res.send("<h1>Deu certo!!!</h1>")
+           res.send("<h1>Deu certo!!!</h1>") 
+        //    precisa retornar uma dica aleatória do banco de dados
         })
 
         app.post("/dicas", (req, res) => {
             res.status(200).json({nome : "Flávio"})
+            //  recebe uma dica no banco de dados
         })
     }
 }
