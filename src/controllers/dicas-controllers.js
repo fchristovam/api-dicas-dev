36f3a4d8e107ca8c9d1489558bc2b6c8a077ab51
fchristovam <fchristovam7@gmail.com>
2022-07-21 12:38:49 -0300

    Cria método post dentro de rotas no arquivo dicas-controller

--- src/controllers/dicas-controllers.js
+++ src/controllers/dicas-controllers.js
@@ -3,6 +3,10 @@ class Dicas {
         app.get("/dicas", (req, res) => {
            res.send("<h1>Deu certo!!!</h1>")
         })
+
+        app.post("/dicas", (req, res) => {
+            res.status(200).json({nome : "Flávio"})
+        })
     }
 }
 
